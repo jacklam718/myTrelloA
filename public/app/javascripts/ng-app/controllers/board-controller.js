e54dec744eff0e2e8379f91f6509f76c2770977a
JackLam <jacklam718@gmail.com>
2015-04-16 23:02:59 +0800

    added board-controller and board-service on ng-app

--- public/app/javascripts/ng-app/controllers/board-controller.js
+++ public/app/javascripts/ng-app/controllers/board-controller.js
@@ -1,7 +1,33 @@
 "use strict";
+console.log("board");
+function BoardController($scope, $rootScope, BoardService, SERVICE_EVENTS) {
 
-function BoardController($scope, $rootScope, BoardService) {
-  console.log($scope);
+  $scope.init = function() {
+    console.log("init");
+  }
+
+  $scope.setBoard = function(event, board) {
+    $scope.board = board;
+    $scope.$apply();
+  };
+
+  $scope.getBoard = function() {
+    return BoardService.getBoard();
+  }
+
+  $scope.setBoardLists = function(event, boardLists) {
+    $scope.boardLists = boardLists;
+    $scope.$apply();
+  }
+
+  $scope.getBoardLists = function() {
+    return BoardService.getBoardLists();
+  }
+
+  $rootScope.$on(SERVICE_EVENTS.boardUpdated, $scope.setBoard);
+  $rootScope.$on(SERVICE_EVENTS.boardListsUpdated, $scope.setBoardLists);
+  console.log("ssss");
+  BoardService.reload();
 }
 
 myTrello.controller("BoardController", BoardController);
--- public/app/javascripts/ng-app/services/board-service.js
+++ public/app/javascripts/ng-app/services/board-service.js
@@ -1,7 +1,37 @@
 "use strict";
 
-function BoardService($rootScope) {
+function BoardService($rootScope, $routeParams, SERVICE_EVENTS) {
+  var self = this;
 
+  this.setBoard = function(board) {
+    self.baord = board;
+
+    $rootScope.$broadcast(SERVICE_EVENTS.boardUpdated, board);
+  };
+
+  this.getBoard = function() {
+    return self.baord;
+  };
+
+  this.setBoardLists = function(boardLists) {
+    self.boardLists = boardLists;
+
+    $rootScope.$broadcast(SERVICE_EVENTS.boardListsUpdated, boardLists);
+  };
+
+  this.getBoardLists = function() {
+    return self.boardLists;
+  };
+
+  this.reload = function() {
+    MyTrello.board($routeParams.id, function(board) {
+      self.setBoard(board);
+    })
+
+    MyTrello.boardLists($routeParams.id, function(boardLists) {
+      self.setBoardLists(boardLists);
+    });
+  }
 }
 
 myTrello.service("BoardService", BoardService);
