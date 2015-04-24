ab2a3d8c0e27b9701f65dce1407407b421016012
JackLam <jacklam718@gmail.com>
2015-04-24 19:10:06 +0800

    removed webhook

--- public/app/javascripts/app.js
+++ public/app/javascripts/app.js
@@ -1,25 +1,3 @@
 $(document).ready(function() {
   MyTrello.login();
-
-  var trelloAccounts = localStorage.getItem("trelloAccounts");
-  var currentAccount = {};
-
-  MyTrello.get("/members/me/", function(me) {
-    if (trelloAccounts) {
-      trelloAccounts = JSON.parse(trelloAccounts);
-      currentAccount = trelloAccounts[me.id];
-    } else {
-
-      MyTrello.webHook("webhooks for me", "http://my-trello.herokuapp.com/trelloCallback", me.id, function(response) {
-        currentAccount[me.id] = {
-          userId: me.id,
-          token: MyTrello.token(),
-          webHookId: response.id,
-          hasWebhooke: true
-        }
-
-        localStorage.setItem("trelloAccounts", JSON.stringify(currentAccount));
-      })
-    }
-  })
 })
